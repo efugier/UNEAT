649c45362407e8a572dba36090a0e95858f1d052
EFugier <emilienfugier@gmail.com>
2018-01-22 16:58:48 +0100

    added obstacles

--- src/uicorn.py
+++ src/uicorn.py
@@ -1,11 +1,11 @@
 # Simple game which is to be solved by the NEAT algorithm
 
-from tkinter import *
+import tkinter as tk
 from random import random, randint
 
 # PARAMETERS
 
-canh, canw = 350, 1000         # canva dimensions in px
+canh, canw = 350, 1000         # canvas dimensions in px
 
 max_nb_jumps = 3               # max number of consecutive jumps
 dt = 50                        # time step in ms
@@ -14,8 +14,7 @@ dt = 50                        # time step in ms
 # CLASSES
 
 class Box:
-    """A simple box object
-       Box can collide with each others"""
+    """A simple box object with can collide with others"""
 
     def __init__(self, x, y, w, h):
         """(x,y) is the top-left corner of the box"""
@@ -35,9 +34,13 @@ class Box:
             return False
         return True
 
+    def reposition(self, newx, newy):
+        self.x, self.y = newx, newy
+        self.x2, self.y2 = newx + self.w, newy + self.h
+
 
 class DrawableBox(Box):
-    """A bow that can be drawn on a canva"""
+    """A bow that can be drawn on a canvas"""
 
     def __init__(self, x, y, w, h, can, obj=None, color='violet'):
         Box.__init__(self, x, y, w, h,)
@@ -47,5 +50,22 @@ class DrawableBox(Box):
         else:
             self.obj = can.create_rectangle(x, y, self.x2, self.y2, fill=color)
 
-    def redraw(self, newx, newy):
+    def reposition(self, newx, newy):
+        Box.reposition(self, newx, newy)
         self.can.coords(self.obj, newx, newy, newx + self.w, newy + self.h)
+
+    def erase(self):
+        self.can.delete(self)
+
+
+class Obsatcle(DrawableBox):
+    """An obstacle which the unicorn lust avoid"""
+
+    def __init__(self, x, y, w, h, can, obj=None, color='violet'):
+        DrawableBox.__init__(self, x, y, w, h, can, None, color='violet')
+
+    def update(self, v):
+        """updates the box going left at a speed v"""
+        self.reposition(self.x - v * dt, self.y)
+        if self.x < -self.w:  # removes the box if it is out of the screen
+            self.erase()
